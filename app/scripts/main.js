87f01003681ec86abc52a6da85a2dd9b5431cfcd
Andrew Pal <andrew.pal@gmail.com>
2015-10-19 11:58:57 -0400

    adjustments for hero image at low and high resolutions; make disclaimer text larger; fix scroll issue in mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,37 @@
 (function($) {
-	$('.nav a').on('click', function(){
-		$('.navbar-collapse').collapse('hide');
-	});
+
+$("#nav ul li a[href^='#']").on('click', function(e) {
+
+	// prevent default anchor click behavior
+	e.preventDefault();
+
+	// store hash
+	var hash = this.hash;
+
+	function navigate() {
+		// animate
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 300, function(){
+			window.location.hash = hash;
+		});
+	}
+
+	if($('.navbar-toggler').is(':visible')) {
+
+		$('#navbar-collapse').one('hidden.bs.collapse', function () {
+			navigate();			 		
+		});
+
+		// collapse mobile nav first
+	 	var x = $('#navbar-collapse').collapse('hide');		
+
+	}
+	else {
+		navigate();
+	}
+
+
+});
+
 }(jQuery));
