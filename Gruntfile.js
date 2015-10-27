bbd6cc39adc6205581354a09c27026b2463b6f1e
Andrew Pal <andrew.pal@gmail.com>
2015-10-27 11:45:26 -0400

    stop removing attribute quotes in minified html

--- Gruntfile.js
+++ Gruntfile.js
@@ -282,7 +282,7 @@ module.exports = function (grunt) {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
           conservativeCollapse: true,
-          removeAttributeQuotes: true,
+          removeAttributeQuotes: false,
           removeCommentsFromCDATA: true,
           removeEmptyAttributes: true,
           removeOptionalTags: true,
